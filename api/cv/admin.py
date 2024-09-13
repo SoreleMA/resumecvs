16e7ec384b7e164d8f41fee9e8dfbc879c0409df
SoreleMA <ada_hack20@hackwomen.dev>
2024-09-13 19:58:46 -0400

    Se realizo los administradores de paneles para django de users y cv

--- api/cv/admin.py
+++ api/cv/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Curriculum, Education, Experience
 
-# Register your models here.
+class EducationInline(admin.TabularInline):
+    model = Education
+    extra = 1
+
+class ExperienceInline(admin.TabularInline):
+    model = Experience
+    extra = 1
+
+class CurriculumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_name', 'email', 'phone', 'created_date')
+    search_fields = ('name', 'last_name', 'email', 'phone')
+    inlines = [EducationInline, ExperienceInline]
+
+
+admin.site.register(Curriculum, CurriculumAdmin)
+admin.site.register(Education)
+admin.site.register(Experience)
--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from django.forms import ModelForm
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+#Formulario para crear usuarios
+class UserCreationForm(ModelForm):
+
+    class Meta:
+        model = User
+        field = ('email', 'name', 'role')
+
+def save(self, commit=True):
+    user = super().save(commit=False)
+    user.set_password(self.cleaned_data['password'])
+    if commit:
+        user.save()
+    return user
+
+#Formulario para actualizar usuarios
+class UserChangeForm(ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'name', 'role', 'is_active', 'is_staff')
+
+#Formulario para agregar y cambiar usuarios
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+#Los Campos a mostrar en el panel de administración
+
+    list_display = ('email', 'name', 'role', 'is_active', 'is_staff')
+    list_filter = ('role', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields':('email', 'password')}),
+        ('Informacion Personal', {'fields': ('name', 'role')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'role', 'password1', 'password2')
+        }),
+    )
+
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+    filter_horizontal = ()
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
