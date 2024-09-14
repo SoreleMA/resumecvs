4cf5d84c1c0dd816d82c1a612b21afec5a11f55f
SoreleMA <ada_hack20@hackwomen.dev>
2024-09-13 20:09:58 -0400

    Se realizo cambios en admin users

--- api/users/admin.py
+++ api/users/admin.py
@@ -45,7 +45,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'name', 'role', 'password1', 'password2')
+            'fields': ('email', 'name', 'role')
         }),
     )
 
